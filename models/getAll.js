c48ebbe5cfe15fbf08bb235c66c85ec453f2ef1f
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-11-06 17:45:21 +0200

    Revert "newChange"
    This reverts commit 5f7cc61856b437a06f2e6710ee3e854dc9efdc64.

--- models/getAll.js
+++
@@ -1,7 +0,0 @@
-const {Contact} = require("./contacts");
-const getAll = async (req,res) => {
-    const contacts = await Contact.find();
-    res.json(contacts);
-    return contacts;
-}
-module.exports = getAll;
