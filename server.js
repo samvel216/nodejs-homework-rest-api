b396d9a276162784a4b567aa5ae7757b973a7950
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-11-18 14:58:22 +0200

    finish

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 mongoose.connect(DB_HOST).then(() => console.log("DataBase connect"))
 .catch(error => console(error.message));
 
-
+console.log(process.env);
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
